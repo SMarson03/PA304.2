d0f3e41fd928e615c4131ed3dee43eb4ff57827c
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-10-07 22:33:47 -0400

    1

---
+++ PythonNumberDatatype.py
@@ -0,0 +1,73 @@
+# Declaring two integer variables
+a = 5
+b = 10
+
+# Adding them together
+sum_integers = a + b
+
+# Printing the result
+print("Sum of integers:", sum_integers)
+
+# Declaring two float variables
+x = 5.5
+y = 10.3
+
+# Adding them together
+sum_floats = x + y
+
+# Printing the result
+print("Sum of floats:", sum_floats)
+
+# Declaring an integer variable and a float variable
+integer_value = 5
+float_value = 10.5
+
+# Adding them together
+sum_mixed = integer_value + float_value
+
+# Printing the result
+print("Sum of integer and float:", sum_mixed)
+
+# The variable type of the sum is a float.
+
+# Declaring two integer variables
+num1 = 20
+num2 = 5
+
+# Dividing the larger number by the smaller number
+if num1 > num2:
+    result_integer_division = num1 / num2
+else:
+    result_integer_division = num2 / num1
+
+# Printing the result
+print("Result of integer division:", result_integer_division)
+
+# Product prices
+coffee_price = 2.50
+cappuccino_price = 3.75
+espresso_price = 2.00
+
+# Calculating subtotal for the order
+subtotal = (3 * coffee_price) + (4 * cappuccino_price) + (2 * espresso_price)
+
+# Sales tax
+SALES_TAX = 0.08
+totalSale = subtotal + (subtotal * SALES_TAX)
+
+# Printing the results
+print("Subtotal:", subtotal)
+print("Total Sale (with tax):", totalSale)
+
+# Radius of the circle
+radius = 63
+
+# Calculating the area of the circle
+pi = 3.14
+area = pi * (radius ** 2)
+
+# Printing the result
+print("Area of the circle:", area)
+
+
+
